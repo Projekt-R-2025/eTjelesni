271483b27b3d350f6f7d4859053d2996f9a4897d
AndreaA44 <andrea.andelkovic@fer.unizg.hr>
2025-10-28 22:51:48 +0100

    add starting route to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import './App.css'         // Msn da nije potrebno vise
+import Login from "./components/Login";
+import Home from "./components/Home";
+/* import Bike from './components/Bike'; */
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  //Provjera autentifikacije pri ucitavanju
+  useEffect(() => {
+    const checkAuth = () => {
+      const userStr = sessionStorage.getItem("user");
+      setIsAuthenticated(!!userStr);
+    };
+    checkAuth();
+  }, []);
+
+  const handleAuthentication = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    sessionStorage.removeItem("user");
+  };
+
+  // Komponenta za zasticene rute
+  const ProtectedRoute = ({ children }) => {
+    const userStr = sessionStorage.getItem("user");
+    if (!userStr) {
+      return <Navigate to="/" />;
+    }
+    return children;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Root route za autentifikaciju (Login) */}
+        <Route 
+          path="/" 
+          element={
+            isAuthenticated ? (
+              <Navigate to="/home" replace />
+            ) : (
+              <Login onAuthenticate={handleAuthentication} />
+            )
+          }
+        />
+
+        {/* Zasticene rute */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home onLogout={handleLogout} />
+            </ProtectedRoute>
+          }
+        />
+
+
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
