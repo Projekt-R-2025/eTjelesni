d090100b0d6833915ffc2332d1266191896187ba
Matej Stupljanec <matej.stupljanec@gmail.com>
2025-12-14 17:13:55 +0100

    remove key

--- frontend/src/components/MapRoute.jsx
+++ frontend/src/components/MapRoute.jsx
@@ -38,7 +38,7 @@ const MapRoute = ({ hoveredAd }) => {
 
     return (
         <div>
-            <LoadScript googleMapsApiKey="REMOVED_API_KEY">
+            <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
                 <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
                     {hoveredAd && hoveredAd.A && hoveredAd.B && (
                         <DirectionsService
