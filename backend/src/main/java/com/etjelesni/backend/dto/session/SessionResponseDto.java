09e7bb5a924194c308563e95eed46ba5111bf936
Matej Stupljanec <matej.stupljanec@gmail.com>
2026-01-04 12:14:10 +0100

    [feature] new attributes for session

--- backend/src/main/java/com/etjelesni/backend/dto/session/SessionResponseDto.java
+++ backend/src/main/java/com/etjelesni/backend/dto/session/SessionResponseDto.java
@@ -6,10 +6,14 @@ import java.time.LocalDateTime;
 @Data
 public class SessionResponseDto {
     private Long id;
+    private String title;
+    private String description;
     private Integer capacity;
     private Integer points;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+    private String startLocation;
+    private String endLocation;
     private Long sectionId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
--- backend/src/main/java/com/etjelesni/backend/model/Notification.java
+++ backend/src/main/java/com/etjelesni/backend/model/Notification.java
@@ -3,7 +3,6 @@ package com.etjelesni.backend.model;
 import com.etjelesni.backend.enumeration.NotificationType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
