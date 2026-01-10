71fde29460364e36be6c62939eb1b40d3627778b
nina zamberlin <nz56224@fer.hr>
2026-01-10 13:01:05 +0100

    updated user (#39)

--- backend/src/main/java/com/etjelesni/backend/service/SectionService.java
+++ backend/src/main/java/com/etjelesni/backend/service/SectionService.java
@@ -26,6 +26,7 @@ public class SectionService {
     private final SectionLeaderRepository sectionLeaderRepository;
 
     private final CurrentUserService currentUserService;
+    private final UserService userService;
 
     public List<SectionResponseDto> getAllSections() {
         List<Section> sections = sectionRepository.findAll();
@@ -77,6 +78,8 @@ public class SectionService {
         sectionLeader.setLeader(user);
         sectionLeader.setSection(section);
         sectionLeaderRepository.save(sectionLeader);
+
+        userService.updateLeadingSections(user, section.getId());
     }
 
 }
